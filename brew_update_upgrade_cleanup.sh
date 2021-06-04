2df1e351c668c8fe6e3f87b8cc75cfd06982e730
Jess Moore <jessica.moore@anu.edu.au>
2021-06-04 14:10:10 +1000

    added this useful command on one line as a script

---
+++ brew_update_upgrade_cleanup.sh
@@ -0,0 +1,13 @@
+# /bin/sh brew_update.sh
+
+echo("This script runs update, upgrade, cleanup, prune, cu -ay, cask cleanup \n\n")
+
+brew update && brew upgrade && brew cleanup && brew prune && brew cu -ay && brew cask cleanup
+
+# brew update updates Homebrew itself and download the last version of the formulae
+# brew upgrade updates the packages you have installed that have new formulae.
+# brew cleanup cleans the cache of old versions of packages.
+# brew prune clean the old symbolic links form `/usr/bin/`.
+# brew cu -ay uses buo/homebrew-cask-upgrade to update casks. -ay flag is all and yes update all outdated apps.
+# brew cask cleanup clean the old caches of the updated apps.
+# If for some reason you don’t want to update an specific package in Homebrew, you can _pin _in to an specific version or the current version.
